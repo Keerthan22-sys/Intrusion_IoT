c84e1887b4dbbea66cac79c88924648ffb659f34
Keerthan <keerthangowdas222@gmail.com>
2024-11-09 14:40:15 +0530

    added API key, logic to check zip code, mapping the response to our interface

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -12,10 +12,12 @@ import { CurrentWeather } from './current-weather';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = "Weather-APP";
   // Task 1: Declare your variable here
-
+  userInput: string = 'London';
   // Task 4: Declare your variables here
-  
+  $data!: Observable<CurrentWeather>;
+  errorMessage: string = '';
   // Task 6: Declare your variables here
 
   // Task 7: Declare your variables here
@@ -30,9 +32,34 @@ export class AppComponent {
 
   searchWeather() {
     // Task 3: Select API URL here
-
+ // Remove spaces from the user's input to simplify detection
+ const sanitizedInput = this.userInput.replace(/\s+/g, '');
+ const zipCodePattern = /^\d{5}(?:-\d{4})?$/; // ZIP code pattern // regex expression
+ const isZipCode = zipCodePattern.test(this.userInput);
+ // Construct the API URL based on the input type
+ let queryParam: string = isZipCode? "zip" : "q"
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?${queryParam}=${this.userInput}&units=metric&appid=${environment.key}`;
     // Task 4: Add your logic here
-
+    this.errorMessage = ''
+    this.$data = this.http.get(apiUrl).pipe(
+      map((data: any) => {
+        return {
+          visibility: data.visibility,
+          windSpeed: data.wind.speed,
+          degTemp: data.main.temp,
+          feelsLikeTemp: data.main.feels_like,
+          name: data.name,
+          dt: this.datePipe.transform(new Date(data.dt * 1000), 'medium'),
+          description: data.weather[0].description,
+          country: data.sys.country,
+        };
+      }),
+      catchError((error) => {
+        this.errorMessage = error.error.message;
+ 
+        return throwError('An error occurred while fetching weather data.');
+      })
+    );
     // Task 6: Add your logic here
 
     // Task 8: Empty the date data here
